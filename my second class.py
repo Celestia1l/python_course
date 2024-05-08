90c17d62fdfa7558ea987adcc4e67a5910a813b1
konok <konokirill1@gmail.com>
2024-05-08 20:23:12 +0300

    test

---
+++ my second class.py
@@ -0,0 +1,18 @@
+class Warrior:
+    def __init__(self, weapons, armor, state):
+        self.weapons = weapons
+        self.armor = armor
+        self.state = state
+        print(self)
+
+    def attack(self):
+        print((f'Воин {self.armor} {self.weapons} {self.state} атакует оппонента'))
+    def retreat(self):
+        print((f'Воин {self.armor} {self.weapons} {self.state} отступает с поля битвы'))
+
+samurai_1 = Warrior('катана','пластинчитые доспехи', 'Япония')
+print(samurai_1)
+print(id(samurai_1))
+print(samurai_1.weapons, samurai_1.armor, samurai_1.state)
+samurai_1.attack()
+samurai_1.retreat()
