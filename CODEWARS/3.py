0d83ca43142f5b2671ad37ca88e9cbf215566ad1
konok <konokirill1@gmail.com>
2024-05-10 14:34:52 +0300

    test

--- CODEWARS/3.py
+++ CODEWARS/3.py
@@ -3,7 +3,7 @@
 # In this kata you need to check the provided array (x) for good ideas 'good' and bad ideas 'bad'.
 # If there are one or two good ideas, return 'Publish!', if there are more than 2 return 'I smell a series!'.
 # If there are no good ideas, as is often the case, return 'Fail!'.
-def well(x: str) -> str:
+def well(x: list) -> str:
     k=0
     for i in x:
         if i == 'good':
