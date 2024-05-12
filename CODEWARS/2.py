941cf877ccb63ca01fc1c5fc6ccf626071985f31
konok <konokirill1@gmail.com>
2024-05-12 12:38:57 +0300

    test

--- CODEWARS/2.py
+++ CODEWARS/2.py
@@ -1,7 +1,12 @@
-#https://www.codewars.com/kata/5966e33c4e686b508700002d
-#Create a function that takes 2 integers in form of a string as an input, and outputs the sum (also as a string
+# https://www.codewars.com/kata/5966e33c4e686b508700002d
+# Create a function that takes 2 integers in form of a string as an input, and outputs the sum (also as a string
+
+def str_to_int(s: str) -> int:
+    return int(s) if s.isdigit() else 0
+
+
 def sum_str(a: str, b: str) -> str:
-    a = int(a) if a != '' else 0
-    b = int(a) if a != '' else 0
-    s=int(a)+int(b)
-    return str(s)
+    return str_to_int(a) + str_to_int(b)
+
+
+print(sum_str('x', '123'))
