d9bd1a26e4c1a21fa54febf7c11c59d6daf835e0
konok <konokirill1@gmail.com>
2024-05-15 15:55:15 +0300

    test

--- class math.py
+++ class math.py
@@ -13,6 +13,30 @@ class Math:
             s += i
         return s
 
-match_1 = Math
-print(match_1.addition(5,10,20))
-print(match_1.multiplication(5,10,20))
+    @staticmethod
+    def division(*args) -> float:
+        s = args[0]
+        for i in args:
+            s /= i
+        return s * args[0]
+
+    print(division(100, 50, 2))
+
+    @staticmethod
+    def subtraction(*args) -> float:
+        s = args[0]
+        for i in args:
+            s -= i
+        return s + args[0]
+
+    print(subtraction(100, 50, 25, 5))
+
+    @staticmethod
+    def factorial(s) -> float:
+        q = 1
+        for i in range(1, s):
+            s *= i
+        return s
+
+    print(factorial(5))
+
