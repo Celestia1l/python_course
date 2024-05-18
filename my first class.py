c576ef10a5c04398e65dc1be5fa2f11a6a59ffa8
konok <konokirill1@gmail.com>
2024-05-18 23:38:36 +0300

    test

--- my first class.py
+++ my first class.py
@@ -13,9 +13,41 @@ class VideoCard:
         self.interface = interface
 
 
+class Processor:
+    def __str__(self) -> str:
+        return f'процессор {self.brand} {self.model} {self.cores}\n'
+
+    def __init__(self, brand: str, model: str, cores: int):
+        self.brand = brand
+        self.model = model
+        self.cores = cores
+
+
+class Cooling_system:
+    def __str__(self):
+        return f'система охлаждения {self.brand} {self.type} {self.noise}\n'
+
+    def __init__(self, brand: str, noise: str, type: str):
+        self.brand = brand
+        self.noise = noise
+        self.type = type
+
+
+class Motherboard:
+    def __srt__(self):
+        return f'Материнская плата {self.brand} {self.model} {self.socket} {self.chipset}\n'
+
+    def __init__(self, brand, model, socket, chipset):
+        self.brand=brand
+        self.model=model
+        self.socket=socket
+        self.chipset=chipset
+
+
 class Computer:
-    button ='on'
-    def __init__(self, video_card: VideoCard, processor, cooling_system, motherboard):
+    button = 'on'
+
+    def __init__(self, video_card: VideoCard, processor: Processor, cooling_system: Cooling_system, motherboard):
         self.video_card = video_card
         self.processor = processor
         self.cooling_system = cooling_system
@@ -30,14 +62,13 @@ class Computer:
             f'Компьютер {self.video_card} {self.processor} {self.cooling_system} {self.motherboard} переустанавливает драйвера')
 
     @classmethod
-    def turns_on(cls,x: int) -> int:
+    def turns_on(cls, x: int) -> int:
         return (f'Компьтер включается после нажатия кнопки {cls.button}')
 
     @staticmethod
     def Mouse_responsiveness_depending_on_click_speed(x: float, l=0.001) -> float:
-        s=x+l
+        s = x + l
         return print((f'скорость отклика мыши с учетом быстроты нажатия пользователя равна {s}'))
-
 
 
 video = VideoCard('acer', '3050', 32, 'pci-x')
@@ -47,8 +78,5 @@ print(id(comp_1))
 print(comp_1.video_card, comp_1.processor, comp_1.cooling_system, comp_1.motherboard)
 comp_1.update()
 comp_1.reinstalls_drivers()
-comp_2 = comp_1
-comp_2.processor = 'xxxx'
-print(comp_1.processor)
 print(comp_1.turns_on('on'))
 print(comp_1.Mouse_responsiveness_depending_on_click_speed(1))
