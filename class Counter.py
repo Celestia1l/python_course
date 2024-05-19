cd6669fe62d6db87738e9511903f2db72eb7f49e
konok <konokirill1@gmail.com>
2024-05-19 12:10:59 +0300

    добавил метод и объект класса

--- class Counter.py
+++ class Counter.py
@@ -3,10 +3,24 @@ class Counter:
 
     @classmethod
     def increment(cls):
-        cls.count +=1
+        cls.count += 1
         return cls.count
 
+    def increment_2(self):
+        self.count += 1
+        return self.count
 
+
+count_1 = Counter()
 print(Counter.increment())
 print(Counter.increment())
+print(count_1.increment_2())
 print(Counter.increment())
+print(count_1.increment_2())
+
+# C:\Users\konok\PycharmProjects\111111\.venv\Scripts\python.exe "C:\Users\konok\python_course\class Counter.py"
+# 1
+# 2
+# 3
+# 3
+# 4
